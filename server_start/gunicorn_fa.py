8c47fe26cd66a163ec68c879c349ff2eb8dbf3a8
Valentin <quitevi@gmail.com>
2024-04-10 14:44:58 +0300

    upd: FastAPI gunicorn config

--- server_start/gunicorn_fa.py
+++ server_start/gunicorn_fa.py
@@ -3,8 +3,8 @@ from uvicorn.workers import UvicornWorker
 
 class MyUvicornWorker(UvicornWorker):
     CONFIG_KWARGS = {
-        "loop": "asyncio",  # auto | asyncio | uvloop
-        "http": "h11",  # auto | h11 | httptools
+        "loop": "uvloop",  # auto | asyncio | uvloop
+        "http": "httptools",  # auto | h11 | httptools
         "lifespan": "off",
         "workers": 1,
         "interface": "asgi3",  # auto | asgi3 | asgi2 | wsgi
@@ -19,6 +19,5 @@ command = "./venv/bin/gunicorn"
 pythonpath = "."
 bind = ":8002"
 workers = 4
-# raw_env = "DJANGO_SETTINGS_MODULE=async_try.settings"
 errorlog = "./logs/gunicorn_fa.log"
-# max_requests = 100
+keepalive = 120
